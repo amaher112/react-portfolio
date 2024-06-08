79897a0a9ed5c7225941022077a15b0cf249998a
amaher <amaher112@su.edu>
2024-06-07 20:58:57 -0400

    'added resume, screenshot and code to resume page'

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,8 +1,18 @@
 import styled from 'styled-components';
 
+const StyledButton = styled.button`
+// background-color: #535bf2;
+
+`
+
 function Resume() {
     return (
-        
+        <>
+        {/* <img src='src/assets/maher-resume.png' alt='resume' width='40%' height='40%'></img> */}
+        <StyledButton>
+        <a href='assets/resume.pdf' download>Click here to download my resume</a>
+        </StyledButton>
+        </>
     )
 }
 
