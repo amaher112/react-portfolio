dc2ba75aadc515232abb5082f01d77e522811c5a
amaher <amaher112@su.edu>
2024-07-02 18:13:57 -0400

    uploaded new resume

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -28,7 +28,7 @@ function About() {
       </p>
       {/* <Line />   */}
       <StyledButton>
-        <a href='/images/resume.pdf' download>Click here to download my resume</a>
+        <a href='/images/Anne-Michele-resume.pdf' download>Click here to download my resume</a>
         </StyledButton>
     </>
   );
